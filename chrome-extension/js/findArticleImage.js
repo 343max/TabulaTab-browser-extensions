adc82edccec46c4a53fe4bf84678560aa62767b2
Max Winde <max.winde@gmail.com>
2011-08-30 15:08:47 +0200

    finds & prefers facebook og:image's

--- chrome-extension/js/findArticleImage.js
+++ chrome-extension/js/findArticleImage.js
@@ -1,109 +1,129 @@
 window.setTimeout(function() {
-	var images = document.getElementsByTagName('img');
+	var findImageInPage = function() {
+		var images = document.getElementsByTagName('img');
 
-	var bestMatch = null;
-	var bestArea = 0;
+		var bestMatch = null;
+		var bestArea = 0;
 
-	var adSizes = [
-		{width: 728, height: 90},
-		{width: 468, height: 60},
-		{width: 120, height: 600},
-		{width: 160, height: 600},
-		{width: 200, height: 200},
-		{width: 250, height: 250},
-		{width: 300, height: 250},
-		{width: 336, height: 280}
-	];
+		var adSizes = [
+			{width: 728, height: 90},
+			{width: 468, height: 60},
+			{width: 120, height: 600},
+			{width: 160, height: 600},
+			{width: 200, height: 200},
+			{width: 250, height: 250},
+			{width: 300, height: 250},
+			{width: 336, height: 280}
+		];
 
-	var isHidden = function(el) {
-		try {
-			if (computedStyle.getPropertyValue('display') == 'hidden') {
-				return true;
+		var isHidden = function(el) {
+			try {
+				if (computedStyle.getPropertyValue('display') == 'hidden') {
+					return true;
+				}
+			} catch(e) {
+
 			}
-		} catch(e) {
-			
+
+			if (el.parentNode) {
+				return isHidden(el.parentNode);
+			}
+
+			return false;
 		}
 
-		if (el.parentNode) {
-			return isHidden(el.parentNode);
+		var debug = function(image, message, color) {
+
+			if (message) {
+				image.setAttribute('title', message);
+				console.log(message + ' ' + image.src);
+			}
+
+			if (!color) color = 'red';
+			image.style.border = '3px solid ' + color + ' !important';
 		}
 
-		return false;
-	}
+		debug = function() {};
 
-	var debug = function(image, message, color) {
+		for (var i = 0; i < images.length; i++)  {
+			var image = images[i];
 
-		if (message) {
-			image.setAttribute('title', message);
-			console.log(message + ' ' + image.src);
-		}
+			if (image.height == 0) {
+				debug(image, 'no height given');
+				continue;
+			}
 
-		if (!color) color = 'red';
-		image.style.border = '3px solid ' + color + ' !important';
-	}
+			var ratio = image.width / image.height;
+			var area = image.width * image.height;
 
-	debug = function() {};
+			if (ratio < 0.3 || ratio > 5) {
+				debug(image, 'wrong ration of ' + ratio);
+				continue;
+			}
 
-	for (var i = 0; i < images.length; i++)  {
-		var image = images[i];
+			if (image.y > window.innerHeight || image.x > window.innerWidth) {
+				debug(image, 'outside of visible area');
+				continue;
+			}
 
-		if (image.height == 0) {
-			debug(image, 'no height given');
-			continue;
-		}
+			if (area < 40000) {
+				debug(image, 'to small (area: ' + area + ' sq px)');
+				continue;
+			}
 
-		var ratio = image.width / image.height;
-		var area = image.width * image.height;
+			if (image.x == 0 && image.y == 0) {
+				debug(image, 'position: 0,0');
+				continue;
+			}
 
-		if (ratio < 0.3 || ratio > 5) {
-			debug(image, 'wrong ration of ' + ratio);
-			continue;
-		}
+			/*
+			for(var i = 0; i < adSizes.length; i++) {
+				var adSize = adSizes[i];
+				if (image.width == adSize.width && image.height == adSize.height) {
+					debug(image, 'ad: ' + adSize.width + 'x' + adSize.height);
+				}
+			}
+			*/
+			/*
+			if (isHidden(image)) {
+				debug(image, 'is hidden');
+				continue;
+			}
+			*/
 
-		if (image.y > window.innerHeight || image.x > window.innerWidth) {
-			debug(image, 'outside of visible area');
-			continue;
-		}
+			debug(image, 'valid candidate (area: ' + area + ' ratio: ' + ratio + ')', 'yellowgreen')
 
-		if (area < 40000) {
-			debug(image, 'to small (area: ' + area + ' sq px)');
-			continue;
-		}
-
-		if (image.x == 0 && image.y == 0) {
-			debug(image, 'position: 0,0');
-			continue;
-		}
-
-		/*
-		for(var i = 0; i < adSizes.length; i++) {
-			var adSize = adSizes[i];
-			if (image.width == adSize.width && image.height == adSize.height) {
-				debug(image, 'ad: ' + adSize.width + 'x' + adSize.height);
+			if (area > bestArea) {
+				bestArea = area;
+				bestMatch = image;
+			} else {
+				debug(image, 'valid candidate, allready beaten (area: ' + area + ' ratio: ' + ratio + ')', 'yellow')
 			}
 		}
-		*/
-		/*
-		if (isHidden(image)) {
-			debug(image, 'is hidden');
-			continue;
-		}
-		*/
 
-		debug(image, 'valid candidate (area: ' + area + ' ratio: ' + ratio + ')', 'yellowgreen')
+		if (bestMatch) {
+			debug(bestMatch, 'best match: ' + bestArea, 'green');
 
-		if (area > bestArea) {
-			bestArea = area;
-			bestMatch = image;
-		} else {
-			debug(image, 'valid candidate, allready beaten (area: ' + area + ' ratio: ' + ratio + ')', 'yellow')
+			return bestMatch.src;
 		}
 	}
 
-	if (bestMatch) {
-		var image = bestMatch;
-		debug(image, 'best match: ' + bestArea, 'green');
+	var findFacebookImageInPage = function() {
+		var metaTags = document.getElementsByTagName('meta');
 
-		chrome.extension.sendRequest({articleImage: bestMatch.src});
+		for (i = 0; i < metaTags.length; i++) {
+			if (metaTags[i].getAttribute('property') == 'og:image') {
+				console.log('found og:image: ' + metaTags[i].getAttribute('content'));
+				return metaTags[i].getAttribute('content');
+			}
+		}
 	}
+
+	var thumbSrc = findFacebookImageInPage();
+	if (!thumbSrc) thumbSrc = findImageInPage();
+
+	if (thumbSrc) {
+		chrome.extension.sendRequest({articleImage: thumbSrc});
+	}
+
 }, 5000);
