c1d566c800ca95c4170096878f4851361c9b2b75
Max Winde <max.winde@gmail.com>
2012-04-26 22:36:53 +0200

    deleted the chrome extension

--- chrome-extension/launchChromium.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd "/Applications/Chromium.app/Contents"
-./MacOS/Chromium --disable-web-security &
--- chrome-extension/launchTestbrowser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd "/Applications/Google Chrome Canary.app/Contents"
-"./MacOS/Google Chrome Canary" --disable-web-security
